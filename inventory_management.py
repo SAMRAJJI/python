1b8f248cd26610924f9fa0465b8dc0270c8783b0
SamRajji <163744492+SAMRAJJI@users.noreply.github.com>
2024-11-17 11:30:41 +0530

    Update inventory_management.py

--- inventory_management.py
+++ inventory_management.py
@@ -12,9 +12,9 @@ def add():
 def update():
     item = input("Enter the item name to be updated: ")
     if item in inventory:
-        copies = int(input("Enter the quantity to add to the existing quantity: "))
+        copies = int(input("Enter the new quantity: "))
         inventory[item] += copies
-        print("Item updated successfully.\n", inventory)
+        print("Given item is updated successfully.\n", inventory)
     else:
         print("This item does not exist.")
 
@@ -22,16 +22,16 @@ def remove():
     item = input("Enter the item name to be removed: ")
     if item in inventory:
         del inventory[item]
-        print("Item removed successfully.\n", inventory)
+        print("Given item is removed successfully.\n", inventory)
     else:
         print("This item does not exist.")
 
 def display():
-    print("Items in the inventory:\n", inventory)
+    print("The items in the inventory so for:\n", inventory)
 
 def exiting():
     print("Exiting Inventory Management.")
-
+print("INVENTORY MANAGEMENT")
 while True:
     choice = int(input("\n1. Add\n2. Update\n3. Remove\n4. Display\n5. Exit\nEnter your choice number: "))
     if choice == 1:
@@ -46,4 +46,5 @@ while True:
         exiting()
         break
     else:
-        print("Invalid choice. Please try again.")
+        print("Invalid choice.")
+        Print("The inventory at last:\n", inventory)
