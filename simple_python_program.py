fabe15fe2fb976f8daa762ba495979020c3e30a4
SamRajji <163744492+SAMRAJJI@users.noreply.github.com>
2024-11-17 10:57:35 +0530

    Update simple_python_program.py

--- simple_python_program.py
+++ simple_python_program.py
@@ -20,7 +20,7 @@ def calculate():
     
     choice = input("Enter choice (1/2/3/4): ")
     
-     if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
         
